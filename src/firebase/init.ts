2428cae2b1a38696268240faf922d0d6ff59efdf
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2023-07-12 20:20:20 +0100

    troubleshooting deploy issues

---
+++ src/firebase/init.ts
@@ -0,0 +1,24 @@
+// test
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOoG74xQq-wa_mjgidq6gM_olDyHgHA4g",
+  authDomain: "placename-guesser.firebaseapp.com",
+  projectId: "placename-guesser",
+  storageBucket: "placename-guesser.appspot.com",
+  messagingSenderId: "136146579197",
+  appId: "1:136146579197:web:a46e28128dc514e71c9894"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app)
+
+const db = getFirestore()
+export default db 
